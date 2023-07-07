9669749b5551ada63b2f4f26369a7538a7808eea
abby996 <abdiasbrevil@gmail.com>
2023-07-06 21:33:01 -0400

    Added script

--- prove/Develop04/Activty.cs
+++ prove/Develop04/Activty.cs
@@ -1,25 +1,6 @@
 using System.Diagnostics;
 
-class Activity
-{
-    private string description, name;
-    static int spinnerCounter, duration;
-
-
-    public Activity()
-    {
-      spinnerCounter = duration = 0;
-    }
-
-    public void SetActivityName(string _activityName)
-    {
-        name = _activityName;
-    }
-
-    public void SetDescription(string _description)
-    {
-        description = _description;
-    }
+        
 
     public int GetDuration()
     {
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,29 @@
 using System;
 
+namespace employee_demo
+{
+  
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        HourlyEmployee hEmployee = new HourlyEmployee();
+        hEmployee.SetName( "Abdias");
+        hEmployee.SetIdNumber("123abc");
+        hEmployee.SetPayRate(15);
+        hEmployee.SetHoursWorked(35);
+
+        SAlaryEmployee sEmployee = new SAlaryEmployee();
+        sEmployee.SetName("Taina");
+        sEmployee.SetIdNumber("456EJF");
+        sEmployee.SetSalary(60000);
+
+    
+
     }
+    Public static void DisplayEmplyeeInformation(Employee employee)
+    {
+        _employee = employee;
+    }
+}  
 }
