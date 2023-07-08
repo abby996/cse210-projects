060528f956b2b7dd6673350ef3481ac86c8a1d58
abby996 <abdiasbrevil@gmail.com>
2023-07-08 18:23:24 -0400

    Added prove

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -41,13 +41,7 @@ class Program
                     goalManager.ListGoalDetails();
                     break;
 
-                case 3:
-                    goalManager.SaveGoals();
-                    break;
-
-                case 4:
-                    goalManager.LoadGoals();
-                    break;
+                
 
                 case 5:
                     goalManager.RecordEvent();
@@ -57,49 +51,5 @@ class Program
     }
 }
 
-public class GoalManager
-{
-    private List<Goal> _goals;
-    private int _score;
 
-    public GoalManager()
-    {
-        _goals = new List<Goal>();
-        _score = 0;
-    }
 
-    public void SaveGoals()
-    {
-        // Logic to save the goals to a file or database
-        Console.WriteLine("Saving goals...");
-    }
-
-    public void LoadGoals()
-    {
-        // Logic to load the goals from a file or database
-        Console.WriteLine("Loading goals...");
-    }
-
-    public void CreateGoal()
-    {
-        // Logic to create a new goal
-        Console.WriteLine("Creating a new goal...");
-    }
-
-    public void ListGoalDetails()
-    {
-        // Logic to list goal details
-        Console.WriteLine("Listing goal details...");
-    }
-
-    public void RecordEvent()
-    {
-        // Logic to record an event
-        Console.WriteLine("Recording an event...");
-    }
-}
-
-public class Goal
-{
-    // Define properties and methods for a Goal
-}
