4ab78c570ba321d51b40e8acc6005f816060547a
abby996 <abdiasbrevil@gmail.com>
2023-07-07 23:35:14 -0400

    Added prove

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -61,10 +61,9 @@ public class GoalManager
 {
     private List<Goals> _goals;
     private int _score;
-
     public GoalManager()
     {
-        _goals = new List<Goals>();
+        _goals = new List<Goals >();
         _score = 0;
     }
 
